35a7e11376b728529ab100fce095edd3368524fd
angelotacoj <angelotacoj@gmail.com>
2023-04-16 15:24:37 -0500

    Se añadio y completo tres platos con img.

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -7,8 +7,34 @@ const data = {
             image: '/images/ceviche-pescado.jpg',
             price: 20,
             countInStock: 10,
-
-
+            dealership: 'Cafeteria O1',
+            rating: 4.5,
+            numReviews: 5,
+            description: 'Plato de mariscos encebollados y cocidos en limón, acompañados de camote, maíz y cebolla morada.'
+        },
+        {
+            name: 'Papa a la huancaína',
+            slug: 'papa-huancaina',
+            category: 'vegetariano',
+            image: '/images/papa-huancaina.jpg',
+            price: 8,
+            countInStock: 5,
+            dealership: 'Cafeteria F2',
+            rating: 4,
+            numReviews: 3,
+            description: 'Papas cocidas y cortadas en rodajas, bañadas en una salsa de ají amarillo, leche y queso.'
+        },
+        {
+            name: 'Anticuchos de corazón',
+            slug: 'anticucho-corazon',
+            category: 'carnes',
+            image: '/images/anticucho-corazon.jpg',
+            price: 18,
+            countInStock: 20,
+            dealership: 'Cafeteria O1',
+            rating: 5,
+            numReviews: 10,
+            description: 'Brochetas de corazón de res marinado en ajo, comino y ají panca, asadas en parrilla.'
         }
     ]
 }

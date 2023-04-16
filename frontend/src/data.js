3946a0bd4f9d2fa615bb93ccadfa516b3bf7e449
angelotacoj <angelotacoj@gmail.com>
2023-04-16 15:14:05 -0500

    Importación de datos de platos de comida

---
+++ frontend/src/data.js
@@ -0,0 +1,14 @@
+const data = {
+    products: [
+        {
+            name: 'Ceviche de pescado',
+            slug: 'ceviche-pescado',
+            category: 'mariscos',
+            image: '/images/ceviche-pescado.jpg',
+            price: 20,
+            countInStock: 10,
+
+
+        }
+    ]
+}

2adb8bb2c19656c7ad11f1dfc845dd5096d28f3e
Sebas <psvitasebas@gmail.com>
2023-06-10 23:57:53 -0500

    solucion API get Products

--- backend/app.js
+++ backend/app.js
@@ -32,14 +32,6 @@ class App {
         res.status(404).send({ message: "Producto no encontrado" });
       }
     });
-    this.app.get("/api/products/:id", (req, res) => {
-      const product = data.products.find((x) => x._id === req.params.id);
-      if (product) {
-        res.send(product);
-      } else {
-        res.status(404).send({ message: "Producto no encontrado" });
-      }
-    });
   }
 
   setupErrorHandling() {

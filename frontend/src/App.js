6945dfe94670558cbbc2d133c295070ad8a39566
angelotacoj <angelotacoj@gmail.com>
2023-07-02 16:03:28 -0500

    add: inicio de filtro de busqueda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import axios from "axios";
 import PrincipalScreen from './pantallas/PrincipalScreen';
 import DetallesPlatosScreen from './pantallas/DetallesPlatosScreen';
 import LoginScreen from './pantallas/LoginScreen';
-import { useContext } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import { useContext, useEffect, useState } from 'react';
 import { Store } from './Store';
+import { getError } from './utils';
+// import { LinkContainer } from 'react-bootstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
 import RegisterScreen from './pantallas/RegisterScreen';
 import EditarDatosScreen from './pantallas/EditarDatosScreen';
 import Container from 'react-bootstrap/esm/Container';
@@ -14,6 +19,7 @@ import CarritoCompraScreen from './pantallas/CarritoCompraScreen';
 import PagoQR_ExtraSprint2 from './pantallas/PagoQR_ExtraSprint2';
 import BarraBusqueda from './componentes/BarraBusqueda';
 import Footer from './componentes/Footer';
+import Button from 'react-bootstrap/Button';
 
 
 function App() {
@@ -27,10 +33,31 @@ function App() {
         window.location.href = '/login';
     }
 
+    const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+        try {
+            const { data } = await axios.get(`/api/products/categories`);
+            setCategories(data);
+        } catch (err) {
+            toast.error(getError(err));
+        }
+        };
+        fetchCategories();
+    }, []);
+
     return(
         <BrowserRouter>
-        <div>
+        <div className={sidebarIsOpen?'d-flex flex-column site-container active-cont':'d-flex flex-column site-container'}>
             <header className='MenuNav'>
+                <Button
+                    variant="dark"
+                    onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+                >
+                    <i className='fas fa-bars'></i>
+                </Button>
                 <Link to="/">CampusBite</Link>
                     <BarraBusqueda/>
                     <Link to="/cart" className='nav-link'>
@@ -57,6 +84,29 @@ function App() {
                 )}
                 
             </header>
+            <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categorias de platos</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <Link
+                  to={`/search/category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
             <main>
                 <Container className='mt-2'>
                     <Routes>

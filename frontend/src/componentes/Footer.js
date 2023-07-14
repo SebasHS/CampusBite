0945b635b44ff9c31cdb8470ca7ffde09823c76a
SebasHS <psvitasebas@gmail.com>
2023-07-14 12:17:21 -0500

    footer actualizado

--- frontend/src/componentes/Footer.js
+++ frontend/src/componentes/Footer.js
@@ -1,28 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ServiceRestaurante } from '../services/ServiceRestaurante';
 
 const Footer = () => {
   const [horarios, setHorarios] = useState([])
+  const [loading, setLoading] = useState(true);
 
-  const getData =  async () => {
-    try {
-      const {horarios} = await ServiceRestaurante.obtenerHorarios();
-      setHorarios(horarios);
-    } catch (err) {
-      setHorarios(err.message)
-    }
-  }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await ServiceRestaurante.obtenerHorarios();
+        console.log(res)
+        const xd = res.data;
+        setHorarios(xd);
+      } catch (err) {
+        console.error(err)
+      }
+      setLoading(false)
+    };
+
+    getData()
+
+  }, [])
+
+
 
   return (
+
     <div>
-      <h2>Horarios de atención:</h2>
-      <ul>
-        {horas.map((hora, index) => (
-          <li key={index}>
-            {hora.dia}: {hora.horario}
-          </li>
-        ))}
-      </ul> 
+      {loading && <div>Loading</div>}
+      {!loading && (
+        <div>
+          <h2>Horarios de atención:</h2>
+          {horarios.map(item =>
+          (
+            <div>
+              <h3>{item.name}</h3>
+              <ul>
+                <li> Lunes: {item.horario.Lunes}</li>
+                <li> Martes: {item.horario.Martes}</li>
+                <li> Miercoles: {item.horario.Miercoles}</li>
+                <li> Jueves: {item.horario.Jueves}</li>
+                <li> Viernes: {item.horario.Viernes}</li>
+                <li> Sabado: {item.horario.Sabado}</li>
+
+              </ul>
+
+            </div>
+          ))}
+        </div>
+      )}
+
     </div>
   );
 };
